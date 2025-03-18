0ce249c8f2ae21ba52e5626dc75723992d433b4d
Eddy007Saive <laplumenoire001@gmail.com>
2025-03-18 14:47:45 +0300

    Supprimer le dossier

--- React/src/validations/UserSchema.jsx
+++
@@ -1,9 +0,0 @@
-const yup = require('yup');
-
-const userSchema = yup.object({
-  id: yup.number().integer().positive().required(),
-  nom: yup.string().min(3).max(100).required(),
-  email: yup.string().email().required(),
-  mot_de_passe: yup.string().min(6).required(),
-  role: yup.string().oneOf(['utilisateur', 'admin', 'chauffeur']).required(),
-});
--- React/src/validations/VehiculeSchema.jsx
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-const vehiculeSchema = yup.object({
-  marque: yup.string().min(3).max(100).required(),
-  modele: yup.string().min(3).max(100).required(),
-});
-
-export default vehiculeSchema;
